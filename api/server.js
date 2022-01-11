9448ca07eeef902a0d0d4244d8f3d5cdfeaeb1ab
Mina <minasoha@yahoo.com>
2022-01-10 20:28:40 -0500

    get all users works

--- api/server.js
+++ api/server.js
@@ -2,16 +2,27 @@
 
 //imports
 const exprss = require("express");
-const Users = require('./users/model.js')
+const Users = require("./users/model.js");
 
-// Instance of express app
+// Instance of express appp
 const server = exprss();
 
 // Global middleware
-server.use(exprss.json)
-
+server.use(exprss.json());
 
 // End points
-module.exports = {
-    
-}; // EXPORT YOUR SERVER instead of {}
+
+server.get("/api/users", (req, res) => {
+ Users.find()
+  .then((users) => {
+   res.json(users);
+  })
+  .catch((err) => {
+   res.status(500).json({
+    message: "error getting users",
+    err: err.message,
+   });
+  });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
